b9353c6ebc89f304e5c8a3b8ca5f6c655213f2df
Alberto Aragon <alberto@alturasoluciones.com>
2018-06-28 03:33:16 -0500

    Building a Favorite Recipe List

--- src/components/FavoriteRecipeList.js
+++ src/components/FavoriteRecipeList.js
@@ -2,29 +2,32 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import RecipeItem from './RecipeItem';
 
-class RecipeList extends Component {
+class FavoriteRecipeList extends Component {
   render() {
-    // console.log('this.props', this.props);
     return (
       <div>
+        <h4>Favorite Recipes:</h4>
         {
-          this.props.recipes.map((recipe, index) => {
+          this.props.favoriteRecipes.map((recipe, index) => {
             return (
               <RecipeItem
                 key={index}
                 recipe={recipe}
-                favoriteButton={true}
+                favoriteButton={false}
               />
             )
           })
         }
       </div>
+
     )
   }
 }
 
 function mapStateToProps(state) {
-  return state;
+  return {
+    favoriteRecipes: state.favoriteRecipes
+  }
 }
 
-export default connect(mapStateToProps, null)(RecipeList);
+export default connect(mapStateToProps, null)(FavoriteRecipeList);
